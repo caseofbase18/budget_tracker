486668dc3eb45f68b7a78b4e3baa1762ece0a360
Casey Maltese <caseymaltese@gmail.com>
2020-11-01 20:59:16 -0500

    deploying

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget1", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
